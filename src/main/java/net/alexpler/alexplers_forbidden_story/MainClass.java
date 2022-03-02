160ec99c33f6fb41b9421e82f1998158e472c870
AlExpler <shelmanov33@gmail.com>
2022-03-02 10:57:13 +0300

    Added CREDITS.txt Added new Item (Abandoned Crystal). No usage for it for now.

--- src/main/java/net/alexpler/alexplers_forbidden_story/MainClass.java
+++ src/main/java/net/alexpler/alexplers_forbidden_story/MainClass.java
@@ -1,15 +1,17 @@
 package net.alexpler.alexplers_forbidden_story;
 
+import net.alexpler.alexplers_forbidden_story.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainClass implements ModInitializer {
 
+	public static final String MOD_ID = "alexplers_forbidden_story";
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
-		LOGGER.debug("Hello fellow fabric users!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/alexpler/alexplers_forbidden_story/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.alexpler.alexplers_forbidden_story.item;
+
+import net.alexpler.alexplers_forbidden_story.MainClass;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item ABANDONED_CRYSTAL = registerItems(
+            "abandoned_crystal", new Item(new FabricItemSettings().group(ItemGroup.MISC))
+    );
+
+    private static Item registerItems(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(MainClass.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        MainClass.LOGGER.info("Registering Mod Items for " + MainClass.MOD_ID + "...");
+    }
+}
